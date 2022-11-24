0622b9080cae43036e902e93c2232eb893eb9a44
Farhad <fgadirli@turanbank.az>
2022-11-24 15:47:28 +0400

    first unit test

--- test/java/com/company/test/MyMathTest.java
+++ test/java/com/company/test/MyMathTest.java
@@ -4,18 +4,20 @@ import com.company.MyMath;
 import org.junit.jupiter.api.Test;
 
 public class MyMathTest {
+
+    private final MyMath myMath = new MyMath();
+
     @Test
-    public void test() throws IllegalAccessException {
-        int[] numbers = {1, 2, 3};
-        MyMath myMath = new MyMath();
-        int result = myMath.calculateSum(numbers);
-        System.out.println(result);
-        int expectedResult = 6;
-        boolean bool = expectedResult == result;
-        if (!bool) {
+    public void calculateSum_ThreeMemberArray() throws IllegalAccessException {
+        if (!(6 == myMath.calculateSum(new int[]{1, 2, 3}))) {
             throw new IllegalAccessException("Not equal");
         }
-        //Absence of failure is success
-        //Test condition or assert
+    }
+
+    @Test
+    public void calculateSum_ThreeLengthArray() throws IllegalAccessException {
+        if (!(0 == myMath.calculateSum(new int[]{}))) {
+            throw new IllegalAccessException("Not equal");
+        }
     }
 }
